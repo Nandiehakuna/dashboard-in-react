3de2a1d2f839ee89cd35db0aa259d609510248f0
Nandiehakuna <gakiikelly403@gmail.com>
2024-03-02 13:20:11 +0300

    notify icon added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import Logo from "./logo/Logo";
 import Search from "./mini-components/Search";
+import Notification from "./mini-components/Notification";
 
 const Header=()=>{
     return(
         <header className="header-container">
             <Logo/>
             <Search/>
+            <div className="header-end">
+            <Notification/>
+
+            </div>
+         
+
 
         </header>
 
